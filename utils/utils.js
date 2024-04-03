f1bf8bb874fbb4b88d9cdae6224be891d3564ad3
PhilaNgcamu <philangcamu17@gmail.com>
2024-04-03 20:13:07 +0200

    Form Database Schema

--- utils/utils.js
+++ utils/utils.js
@@ -2,12 +2,18 @@ import { SQLite } from "expo-sqlite";
 
 const db = SQLite.openDatabase("bookbuddy.db");
 
-export const initDatabase = () => {
+const initialiseDatabase = () => {
   db.transaction((tx) => {
     tx.executeSql(
-      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, genre TEXT)"
+      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, genre TEXT, available INTEGER)"
+    );
+  });
+
+  db.transaction((tx) => {
+    tx.executeSql(
+      "CREATE TABLE IF NOT EXISTS borrowings (id INTEGER PRIMARY KEY AUTOINCREMENT, book_id INTEGER, borrower_name TEXT, contact_info TEXT, date_borrowed TEXT, date_returned TEXT, status TEXT, FOREIGN KEY (book_id) REFERENCES books(id))"
     );
   });
 };
 
-export default db;
+export { db, initialiseDatabase };

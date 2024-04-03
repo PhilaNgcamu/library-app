74cef75c3b217ab0ebcfdaab452cea32f7948819
PhilaNgcamu <philangcamu17@gmail.com>
2024-04-03 20:16:17 +0200

    Database schema

--- utils/utils.js
+++ utils/utils.js
@@ -16,4 +16,86 @@ const initialiseDatabase = () => {
   });
 };
 
+const addBook = (title, author, genre, available) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "INSERT INTO books (title, author, genre, available) VALUES (?, ?, ?, ?)",
+          [title, author, genre, available],
+          (_, { rowsAffected }) => {
+            if (rowsAffected > 0) {
+              resolve("Book added successfully");
+            } else {
+              reject(new Error("Failed to add book"));
+            }
+          },
+          (_, error) => reject(error)
+        );
+      },
+      (error) => reject(error)
+    );
+  });
+};
+
+const getAllBooks = () => {
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "SELECT * FROM books",
+          [],
+          (_, { rows }) => resolve(rows._array),
+          (_, error) => reject(error)
+        );
+      },
+      (error) => reject(error)
+    );
+  });
+};
+
+const updateBook = (id, title, author, genre, available) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "UPDATE books SET title = ?, author = ?, genre = ?, available = ? WHERE id = ?",
+          [title, author, genre, available, id],
+          (_, { rowsAffected }) => {
+            if (rowsAffected > 0) {
+              resolve("Book updated successfully");
+            } else {
+              reject(new Error("Failed to update book"));
+            }
+          },
+          (_, error) => reject(error)
+        );
+      },
+      (error) => reject(error)
+    );
+  });
+};
+
+const deleteBook = (id) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(
+      (tx) => {
+        tx.executeSql(
+          "DELETE FROM books WHERE id = ?",
+          [id],
+          (_, { rowsAffected }) => {
+            if (rowsAffected > 0) {
+              resolve("Book deleted successfully");
+            } else {
+              reject(new Error("Failed to delete book"));
+            }
+          },
+          (_, error) => reject(error)
+        );
+      },
+      (error) => reject(error)
+    );
+  });
+};
+
 export { db, initialiseDatabase };

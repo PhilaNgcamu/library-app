010ed1d0ee0337038dc07284a50a33fea6f535fc
PhilaNgcamu <philangcamu17@gmail.com>
2024-05-17 20:00:58 +0200

    Added Box Shadow

--- screens/ViewBookScreen.js
+++ screens/ViewBookScreen.js
@@ -236,6 +236,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderRadius: 10,
     padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
   row: {
     flexDirection: "row",
